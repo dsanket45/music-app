156a466148e0f6cfc357d93fa1b6eb7db10f3ccc
DSANKET <dsanket965@gmail.com>
2026-08-25 18:22:56 +0530

    feat: enable background audio playback, wake locks, and native lockscreen player controls

--- android/app/src/main/java/com/sanket/musicapp/MainActivity.java
+++ android/app/src/main/java/com/sanket/musicapp/MainActivity.java
@@ -1,5 +1,21 @@
 package com.sanket.musicapp;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            WebSettings settings = this.bridge.getWebView().getSettings();
+            settings.setMediaPlaybackRequiresUserGesture(false);
+        }
+    }
+}
