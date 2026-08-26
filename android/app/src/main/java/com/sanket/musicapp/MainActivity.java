5f50b174f062f4f7880c156d5c94d3a8abc30826
DSANKET <dsanket965@gmail.com>
2026-08-26 10:40:54 +0530

    fix: override MainActivity onPause and auto-resume background playback in youtubePlayer

--- android/app/src/main/java/com/sanket/musicapp/MainActivity.java
+++ android/app/src/main/java/com/sanket/musicapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.sanket.musicapp;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -14,8 +15,26 @@ public class MainActivity extends BridgeActivity {
     public void onStart() {
         super.onStart();
         if (this.bridge != null && this.bridge.getWebView() != null) {
-            WebSettings settings = this.bridge.getWebView().getSettings();
+            WebView webView = this.bridge.getWebView();
+            WebSettings settings = webView.getSettings();
             settings.setMediaPlaybackRequiresUserGesture(false);
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        // Prevent Android WebView from pausing audio playback when backgrounded
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().resumeTimers();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().resumeTimers();
         }
     }
 }
